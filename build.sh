63e9eae314a82fc60c69645ba26a593c3261c741
David2105 <71578943+DavidReyesCuartas@users.noreply.github.com>
2025-10-19 16:40:39 -0500

    David (#4)
    * Add tickets app with models, serializers, views, and URLs for ticket management
    
    * Add CI workflow for Django project with testing setup
    
    * Fix path to manage.py in CI workflow for Django tests
    
    * Refactor ticket model to use DateTimeField for function, update serializers and views for bought_by field, and add user detail and soft delete views.
    
    * Update CI workflow, add build script, and enhance requirements.txt with additional dependencies
    
    * Fix path to manage.py in build script for static asset collection

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python cinetickets-backend/cinetickets/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python cinetickets-backend/cinetickets/manage.py migrate
